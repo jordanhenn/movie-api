838912c98fad6fee2412a3f11f659a2f04b314f8
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-07-16 20:57:32 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIES = require('./movies.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
+function handleMovies(req, res) {
+    const { genre, country, avg_vote } = req.query;
+    let results = MOVIES;
+
+    if(genre) {
+        results = results.filter(
+            movie => 
+            movie
+            .genre
+            .toLowerCase()
+            .includes(genre.toLowerCase())
+        );
+    }
+
+    if(country) {
+        results = results.filter(
+            movie =>
+            movie
+            .country
+            .toLowerCase()
+            .replace(/\s/g, '')
+            .includes(country.toLowerCase().replace(/\s/g, ''))
+        );
+    }
+
+    if(avg_vote) {
+        results = results.filter(
+            movie =>
+            movie
+            .avg_vote
+            >=
+            Number(avg_vote)
+        );
+    }
+    res.json(results);
+}
+
+app.get('/movie', handleMovies);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+});
